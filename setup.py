ed354b6ab19dfd195cee1117acc8fecb60512757
Tiendil <a.eletsky@gmail.com>
2015-06-08 13:07:32 +0300

    gh-3 Ввести версионность

--- setup.py
+++ setup.py
@@ -2,14 +2,31 @@
 import setuptools
 
 setuptools.setup(
-    name = 'Questgen',
-    version = '0.1.0',
-    author = 'Aleksey Yeletsky',
-    author_email = 'a.eletsky@gmail.com',
-    packages = setuptools.find_packages(),
-    url = 'https://github.com/Tiendil/questgen',
-    license = 'LICENSE',
-    description = "Questgen - generator of nonlenear quests with events and flow validating",
-    long_description = open('README.md').read(),
-    include_package_data = True # setuptools-git MUST be installed
-)
+    name='Questgen',
+    version='0.2.0',
+    description='generator of nonlenear quests with events and flow validating',
+    long_description = open('README.rst').read(),
+    url='https://github.com/Tiendil/questgen',
+    author='Aleksey Yeletsky <Tiendil>',
+    author_email='a.eletsky@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        'Natural Language :: English',
+        'Natural Language :: Russian'],
+    keywords=['gamedev', 'game', 'game development', 'quests', 'quests generation', 'procedural content generation'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    test_suite = 'tests',
+    )
